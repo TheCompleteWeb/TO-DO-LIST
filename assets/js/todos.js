023b2fee06d4094f2460a73944cced816d0a7227
neogeoss <neogeo-s@hanmail.net>
2018-05-22 00:33:38 +0900

    stopping event propagation and remove elements with fadeOut method

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,28 @@ $("li").click(function(){
     $(this).toggleClass("completed");
     
 });
+
+$("span").click(function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    
+    event.stopPropagation();
+});
+
+
+/*
+$("ul").click(function(){
+    alert("clicked on ul");
+    event.stopPropagation();
+});
+
+$("#container").click(function(){
+    alert("clicked on a container div");
+    event.stopPropagation();
+});
+
+$("body").click(function(){
+    alert("Clicked a body");
+    event.stopPropagation();
+});*/
